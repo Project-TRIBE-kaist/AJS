4c44d62be6ba69c87d2030a6b9963735f5d17691
AnJunseo <junseo42@naver.com>
2023-02-04 17:33:03 +0900

    14 online image

--- lib/feed_screen.dart
+++ lib/feed_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tribe_v3/widgets/post.dart';
 
 class FeedScreen extends StatelessWidget {
   //const FeedScreen({Key? key}) : super(key: key);
@@ -24,10 +25,5 @@ class FeedScreen extends StatelessWidget {
 
     );
   }
-  Widget feedListBuilder(BuildContext context, int index){
-    return Container(
-      color: Colors.accents[index % Colors.accents.length],
-      height: 100,
-    );
-  }
+  Widget feedListBuilder(BuildContext context, int index){ return Post(index); }
 }
---
+++ lib/widgets/post.dart
@@ -0,0 +1,26 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class Post extends StatelessWidget {
+  final int index;
+  const Post(this.index, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: 'https://picsum.photos/id/$index/200/300',
+      imageBuilder: (BuildContext context, ImageProvider imageProvider){
+        return AspectRatio(
+          aspectRatio: 0.56,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(image: imageProvider, fit: BoxFit.cover)
+              ),
+            ),
+        );
+      },
+    );
+  }
+}
